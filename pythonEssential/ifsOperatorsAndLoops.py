6d03ad917a0c5b4f5e56b1e818a8672bf841422f
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-07 23:49:43 -0600

    git commit

---
+++ pythonEssential/ifsOperatorsAndLoops.py
@@ -0,0 +1,12 @@
+# python examples of
+# if-elif-else
+# operators
+# loops
+
+# if-elif-else example:
+if True:
+    print('if true')
+elif False:
+    print('elif true')
+else:
+    print('neither true')
