5b528e6fe2617fbcbfe270c7a95409a9ee5f17d2
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-12 14:20:55 -0600

    git commit

---
+++ pythonEssential/dataStructures_2.py
@@ -0,0 +1,25 @@
+# data structures 2
+
+
+# imports ===============================
+
+
+# FUNCTIONS ##################################
+
+
+		
+# main() ================================
+def main():
+	# SETS
+	
+	# List comprehension
+	
+	# Mixed structures
+
+	
+	
+
+# end main()
+if __name__ == '__main__': main()
+
+
