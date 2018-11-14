c405b5f7a031dbc93f529b070b45083e59ba8c58
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-13 18:51:49 -0600

    git commit

--- pythonEssential/dataStructures_2.py
+++ pythonEssential/dataStructures_2.py
@@ -1,8 +1,7 @@
 # data structures 2
-# python has a data type for sets, which are lists that don't allow duplicate elements
 
 # imports ===============================
-
+from math import pi
 
 # FUNCTIONS ##################################
 # sets don't allow duplicates, so printing a set that contains a string
@@ -12,11 +11,22 @@ def print_set(setArg, sortType):
 	if( sortType == 'ordered'):
 		setArg = sorted(setArg)
 	for x in setArg:
-		print(x, end = '')
+		print(x, end = ' ')
+	print()
+
+# print list comprehensions built off a single range
+def print_list(listArg):
+	for x in listArg:
+		print(x, end = ' ')
+	print()
 		
 # main() ================================
 def main():
+	#
 	# SETS
+	#
+	# python has a data type for sets, which are lists that don't allow duplicate elements
+
 	a = set('I really enjoyed reading Stormfront.')
 	b = set('Now I am reading Fool Moon.')
 	
@@ -46,10 +56,33 @@ def main():
 	# return the members in both set a AND set b:
 	print('\nset a & b: ')
 	print_set(a & b, 'ordered')
+
 	
+	#
 	# List comprehension
+	#
+	# a list comprehension is a list created based on another list or iterator
+	# here are some examples of list comprehensions built off a single range
+	seq1 = range(11)
+	seq2 = [(x * 2) for x in seq1]
+	seq3 = [(x) for x in seq1 if x % 3 != 0]
+	seq4 = [(x, x**2) for x in seq1]
+	seq5 = [round(pi, x) for x in seq1]
+	seq6 = {x: x**2 for x in seq1}  # use seq to make a dictionary
 	
+	# print the list comprehensions
+	seqList = [seq1, seq2, seq3, seq4, seq5, seq6]
+	count = 1
+	while ( count <= 6):
+		print('\nseq', count, ': ')
+		print_list(seqList[count-1])
+		count += 1
+	
+	
+	
+	#
 	# Mixed structures
+	# 
 
 	
 	
