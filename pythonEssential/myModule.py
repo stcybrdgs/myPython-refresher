f250dff2d6a49d08fc3ecff1bda754f02831115d
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-18 23:50:36 -0600

    git commit

---
+++ pythonEssential/myModule.py
@@ -0,0 +1,14 @@
+class addNums():
+	def add(self, a, b):
+		sum = a + b
+		return sum
+
+def main():
+	myAddObj = addNums()
+	print(myAddObj.add(1,2))
+	
+	
+
+
+
+if __name__ == '__main__': main()
---
+++ pythonEssential/myModulePrac.py
@@ -0,0 +1,8 @@
+import myModule
+
+def main():
+	myAddObj = addNums()
+	print(myAddObj.add(1,2))
+
+
+if __name__=='__main__':main()
