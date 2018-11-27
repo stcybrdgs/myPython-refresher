0b19a49f465f901de1fb4f26b2cb2e3437ffc288
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-26 22:51:14 -0600

    git commit

---
+++ pythonGUItkinter/useGridMgr.py
@@ -0,0 +1,38 @@
+# useGridMgr.py
+
+# imports ======================================
+from tkinter import *
+from tkinter import ttk        
+
+# tkinter ======================================    
+root = Tk()
+
+# make a bunch of labels for the root window
+colors = {'red', 'orange', 'yellow', 'green', 'blue', 'purple'}
+r = 0
+c = 0
+i = 0
+for color in colors:
+    if( i == 2 ):
+        i = 0
+        c = 0
+        r += 1
+    ttk.Label(root,
+              text = color,
+              background = color
+             ).grid(
+        row = r,
+        column = c
+        )
+    i += 1
+    c += 1
+
+# tk main loop =================================
+root.mainloop()
+
+# main ===============================
+def main():
+    print('Done.')
+
+
+if __name__ == '__main__': main()    
