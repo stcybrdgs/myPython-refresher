e701085f404bbfb89647d906a67875627c8d629b
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-28 09:38:25 -0600

    git commit

--- pythonGUItkinter/keyboardEvents.py
+++ pythonGUItkinter/keyboardEvents.py
@@ -33,8 +33,14 @@ def key_press(event):
     keySymInfo.configure(text = eventKeySym)
     keyCodeInfo.configure(text = eventKeyCode)
 
+def shortcut(action):
+    labelSpecial.configure(text = action)
+    
+    
 # tkinter ======================================    
 root = Tk()
+
+# create label grid to report generic key press info
 labelFrame = ttk.Frame(root)
 labelFrame.grid(row = 0, column = 0, columnspan = 5)
 
@@ -62,9 +68,34 @@ charInfo.grid(row = 1, column = 2)
 keySymInfo.grid(row = 1, column = 3)
 keyCodeInfo.grid(row = 1, column = 4)
 
-# put a listenter on the keyboard
+# put a listenter on the keyboard for a generic key press
 root.bind('<KeyPress>', key_press)
 
+# create a label to display info for special key combinations
+labelSpecial = ttk.Label(root, text = 'Special Keys:')
+labelSpecial.grid(row = 2, column = 0, columnspan = 5)
+
+# listen for special key combinations
+root.bind('<Control-c>', lambda e: shortcut('Special Keys: Copy'))
+root.bind('<Control-v>', lambda e: shortcut('Special Keys: Paste'))
+
+
+'''
+example keyboard event binding statements:
+
+Event Format                    Event Description
+------------------              -------------------
+<Key>, <KeyPress>               User pressed any key
+<KeyPress-Delete>               User pressed Delete key
+<KeyRelease-Right>              User released Right Arrow key
+a, b, c, 1, 2, 3, etc           User pressed a 'printable' key 
+<space>, <less>                 ibid
+<Shift_L>, <Control_R>          User pressed a 'special' key
+<F5>, <Up>                      ibid
+<Return>                        User pressed the Enter key
+<Control-Alt-Next>              User pressed Ctrl+Alt+PageDown keys
+
+'''
 
 
 # tk main loop =================================
