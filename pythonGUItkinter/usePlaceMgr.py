6bacb883a1648ee55867fd18e0587fc09fa30f7c
Stacy Bridges <stcybrdgs@gmail.com>
2018-11-27 22:07:53 -0600

    git commit

---
+++ pythonGUItkinter/usePlaceMgr.py
@@ -0,0 +1,19 @@
+# usePlaceMgr.py
+
+# imports ======================================
+from tkinter import *
+from tkinter import ttk        
+
+# tkinter ======================================    
+root = Tk()
+
+
+# tk main loop =================================
+root.mainloop()
+
+# main ===============================
+def main():
+    print('Done.')
+
+
+if __name__ == '__main__': main()    
