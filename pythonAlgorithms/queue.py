35ed7555e0d435f030174c112beec3459e562142
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-23 12:43:16 -0600

    git commit

---
+++ pythonAlgorithms/queue.py
@@ -0,0 +1,24 @@
+# queue.py
+'''
+-  queues are a FIFO structure that support enqueing and dequeing
+-  enque/deque are constant time operations
+-  queues are fundamental structures with broad practical applications
+   in software development
+
+-  QUEUE EXAMPLES:	 
+-    an example of using a queue is order processing because the FIFO queue ensures
+     that orders will be filled in the order that they are received
+-    another example of using a queue is message processing
+
+'''
+
+# classes =============================
+
+# functions ===========================
+	
+# main ================================
+def main():
+	
+	print('Done.')
+	
+if __name__ == '__main__' : main()
--- pythonAlgorithms/stack.py
+++ pythonAlgorithms/stack.py
@@ -1,9 +1,8 @@
 # stack.py
 '''
 -  stacks are a LIFO structure that support push() and pop()
--  queues are a FIFO structure that support enqueing and dequeing
 -  push/pop and enque/deque are constant time operations
--  stacks and queues are fundamental structures with broad practical applications
+-  stacks are fundamental structures with broad practical applications
    in software development
 
 -  STACK EXAMPLES:
@@ -12,10 +11,6 @@
 -    another example of using a stack is to provide a backtracking feature, such
      as when you follow links in your browser; if you push each link onto a stack,
      then backtracking just requires a pop()
--  QUEUE EXAMPLES:	 
--    an example of using a queue is order processing because the FIFO queue ensures
-     that orders will be filled in the order that they are received
--    another example of using a queue is message processing
 
 '''
 
@@ -29,7 +24,6 @@ def main():
 	# make a stack
 	stack = []
 	print('Stack contents: ', stack)
-
 	
 	# push values onto stack
 	val = 0
