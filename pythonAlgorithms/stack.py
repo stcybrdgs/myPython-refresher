9c09ea1edbff99f23660267254e31e842ddf8f68
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-23 12:22:29 -0600

    git commit

---
+++ pythonAlgorithms/stack.py
@@ -0,0 +1,26 @@
+# stack.py
+'''
+-  stacks are a LIFO structure that support push() and pop()
+-  queues are a FIFO structure that support enqueing and dequeing
+-  push/pop and enque/deque are constant time operations
+-  stacks and queues are fundamental structures with broad practical applications
+   in software development
+
+-  STACK EXAMPLES:
+-    one example of using a stack would be to evaluate a mathematical expression
+     using reverse Polish notation
+-    another example of using a stack is to provide a backtracking feature, such
+     as when you follow links in your browser; if you push each link onto a stack,
+     then backtracking just requires a pop()
+-  QUEUE EXAMPLES:	 
+-    an example of using a queue is order processing because the FIFO queue ensures
+     that orders will be filled in the order that they are received
+-    another example of using a queue is message processing
+   
+
+'''
+
+
+# classes =============================
+# functions ===========================
+# main ================================
