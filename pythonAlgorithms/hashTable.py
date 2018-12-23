73fc8e1ac6f523172769f33fb2f2e9b4dd70ceda
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-23 13:49:11 -0600

    git commit

--- pythonAlgorithms/hashTable.py
+++ pythonAlgorithms/hashTable.py
@@ -25,18 +25,28 @@ hash table CONS:
   enumerate entries that are close to a given key because the data may be spread out
   randomly in the system's memory
   
-
 '''
-# imports =============================
-
-# globals =============================
-
-# classes =============================
-
-# functions ===========================
-
 # main ================================
 def main():
+	# use dictionary constructor to create hash table all at once
+	items1 = dict( {'key0' : 0, 'key1' : 1, 'key2' : 2, 'key3' : 3} )
+	print('items1: ', items1)
+	
+	# use dictionary data type to create hash table by adding items
+	items2 = {}
+	k = 'key'
+	v = 0
+	while( v < 4 ):
+		key = k + str(v)
+		items2[key] = v
+		v += 1
+	print('items2: ', items2)
+	
+	# now if you want to find a value, you can access it via the key
+	print('value 3 is: ', items2['key2'])
+	
+	# in an actual hash table, an algorithm does the mapping and the storage
+	# locations in memory are not necessarily contiguous as they are in this example
 	print('Done.')
 
 if __name__ == '__main__' : main()
