b0a6897395de13f6d20a9e007c83258ee4fc9e8a
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-26 09:49:08 -0600

    git commit

---
+++ pythonAlgorithms/recursionExample_2.py
@@ -0,0 +1,11 @@
+# recursionExample_2.py
+
+# functions ============================
+
+	
+# main =================================
+def main():
+
+	print('Done.')
+
+if __name__ == '__main__' : main()	
