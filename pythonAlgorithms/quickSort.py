42558e92378588d8ef05fdff9b94faab7ebb9d3b
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-29 10:29:14 -0600

    git commit

---
+++ pythonAlgorithms/quickSort.py
@@ -0,0 +1,31 @@
+# quickSort.py
+'''
+- merge sort is a 'divide and conquer' algorithm-- ie, it breaks data down into
+  smaller parts that are easier to work with.
+- it uses recursion to perform sorting
+
+'''
+# globals =============================
+items = [6, 20, 8, 9, 19, 56, 23, 87, 41]
+
+# functions ===========================
+def quickSort(data):
+	# break down the data set ------------------------
+	
+	# merge the data sets ------------------------	
+	
+		
+# main ================================
+def main():
+	global items
+	
+	# run the quick sort
+	quickSort(items)
+	
+
+
+
+	# end
+	print('Done.')
+
+if __name__ == '__main__' : main()	
