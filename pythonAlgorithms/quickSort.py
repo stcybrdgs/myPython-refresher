95f35f69ad010e524d9f22550f1b71f5bafbc7c0
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-29 12:01:06 -0600

    git commit

--- pythonAlgorithms/quickSort.py
+++ pythonAlgorithms/quickSort.py
@@ -7,26 +7,59 @@
 - performs data sorting in place and doesn't need extra memory to do its work
 - worst case scenario occurs on a fully sorted or mostly sorted list, in which case
   the performance can occasionally degrade to quadratic time complexity O(n^2)
-- to do quicksort, your algorithm needs to start by finding the pivot point
+- to do quicksort, your algorithm needs to start by finding the pivot point and
+  then partition the list
 '''
 # globals =============================
-items = [6, 20, 8, 9, 19, 56, 23, 87, 41]
+items = [20, 6, 8, 53, 56, 23, 87, 41, 49, 19]
 
 # functions ===========================
-def quickSort(data):
-	# break down the data set ------------------------
+def quickSort(dataSet, first, last):
+	if first < last:
+		# find the split point
+		pivotIndex = partition(dataSet, first, last)
+		
+		# sort the two partitions
+		quickSort(dataSet, first, pivotIndex-1)
+		quickSort(dataSet, pivotIndex+1, last)
 	
-	# merge the data sets ------------------------	
+def partition(dataValues, first, last):
+	# choose the first item as the pivot value
+	pivotValue = dataValues[first]
 	
+	# establish the upper and lower indices
+	lower = first + 1
+	upper = last
+	
+	# search for the split point
+	done = False
+	'''
+	while not done:
+		# advance lower index
+		x=0
+		# advance upper index
+		x=1
+		# if indices cross, we have found the split point
+		x=2
+	'''
+	# once the split point is found, swap the pivot value into the array
+	temp = dataValues[first]
+	dataValues[first] = dataValues[upper]
+	dataValues[upper] = temp
+	
+	# return the split point index
+	return upper
 		
 # main ================================
 def main():
 	global items
 	
-	# run the quick sort
-	quickSort(items)
+	print(items)
 	
-
+	# run the quick sort
+	quickSort(items, 0, len(items)-1)
+	
+	print(items)
 
 
 	# end
