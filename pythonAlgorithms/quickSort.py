2ec7bb7f89f97c71f1e4de7a0edbaa5e5bc035a3
Stacy Bridges <stcybrdgs@gmail.com>
2018-12-29 11:27:20 -0600

    git commit

--- pythonAlgorithms/quickSort.py
+++ pythonAlgorithms/quickSort.py
@@ -3,7 +3,11 @@
 - merge sort is a 'divide and conquer' algorithm-- ie, it breaks data down into
   smaller parts that are easier to work with.
 - it uses recursion to perform sorting
-
+- generally performs better than merge sort O(n log n)
+- performs data sorting in place and doesn't need extra memory to do its work
+- worst case scenario occurs on a fully sorted or mostly sorted list, in which case
+  the performance can occasionally degrade to quadratic time complexity O(n^2)
+- to do quicksort, your algorithm needs to start by finding the pivot point
 '''
 # globals =============================
 items = [6, 20, 8, 9, 19, 56, 23, 87, 41]
