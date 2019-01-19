da00aecab5a57f3604fe6146bd111e41db006a13
Stacy Bridges <stcybrdgs@gmail.com>
2019-01-19 13:46:38 -0600

    git commit

--- temp/manage.py
+++ temp/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # inclusion for practice django project
+
 import os
 import sys
 
@@ -8,7 +9,9 @@ if __name__ == "__main__":
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
-
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
         try:
             import django
         except ImportError:
